dbc23fe74a2daa12755d8ea5aaab77ebcde75875
André Vieira <vieirahbs@hotmail.com>
2022-06-07 16:57:27 -0300

    Login da aplicação finalizado(ainda sem banco de dados)

---
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ControleEstoque.Web.Models;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl) //Direciona para a página de login
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {//Se os dados digitados pelo usuário ainda estiverem invalidos
+             //apresenta novamente a página de login:
+                return View(login);
+            }
+
+            bool achou = (login.Usuario == "as" && login.Senha == "1234");
+            if (achou)
+            {
+                //Salva os dados nos cookies
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                if (Url.IsLocalUrl(returnUrl))//Se a url for da aplicação, após logado
+                {//irá direcionar para ela.
+                    return Redirect(returnUrl);
+                }
+                else
+                {//Se não, vai para a Home.
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login inválido.");
+            }
+
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Logout()//Método de Logout
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+    }
+}
---
+++ ControleEstoque.Web/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Campo {0} obrigatório!")]
+        [Display(Name = "Usuário")]
+        public string Usuario { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Campo {0} obrigatório!")]
+        public string Senha { get; set; }
+        [Display(Name = "Lembrar-me")]
+        public bool LembrarMe { get; set; }
+
+
+
+    }
+}
